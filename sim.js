931416c9ecb1e1325870c0be56444ceadba1f7cb
Karthikeya-Meesala <mkarthikeya41@gmail.com>
2023-09-20 07:52:20 +0530

    display winner, improve slider input

--- sim.js
+++ sim.js
@@ -30,22 +30,23 @@ class Shape {
     this.image = images[type];
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.originalSpeedX = (Math.random() * 0.2 - 0.1) * 2.5;
-    this.originalSpeedY = (Math.random() * 0.2 - 0.1) * 2.5;
+    this.originalSpeedX = (Math.random() * 0.2 - 0.1) * 1;
+    this.originalSpeedY = (Math.random() * 0.2 - 0.1) * 1;
     this.speedX = this.originalSpeedX;
     this.speedY = this.originalSpeedY;
-    this.width = 32;
-    this.height = 32;
+    this.width = 28;
+    this.height = 28;
     this.canMove = false;
   }
 
   update() {
-    this.moveTowardsOpponent();
     this.x += this.speedX;
     this.y += this.speedY;
 
     if (this.x < 0 || this.x + this.width > canvas.width) this.speedX *= -1;
     if (this.y < 0 || this.y + this.height > canvas.height) this.speedY *= -1;
+
+    this.moveTowardsOpponent();
   }
   
 
@@ -110,8 +111,9 @@ class Shape {
       const dy = closestOpponent.y - this.y;
       const distance = Math.sqrt(dx * dx + dy * dy);
 
-      this.speedX = (dx / distance) * 0.5;
-      this.speedY = (dy / distance) * 0.5;
+      const speedMultiplier = parseFloat(speedSlider.value);
+      this.speedX = (dx / distance) * 0.1 * speedMultiplier;
+      this.speedY = (dy / distance) * 0.1 * speedMultiplier;
     }
   }
 }
@@ -132,23 +134,41 @@ function updateSpeed(speedMultiplier) {
 
 const shapesArray = [];
 
-
-
 function createShapes(type, count) {
-    let delay = 0;
-    for (let i = 0; i < count; i++) {
-      setTimeout(() => {
-        const shape = new Shape(type);
-        shapesArray.push(shape);
-        if (i === count - 1) {
-          setTimeout(() => {
-            shapesArray.forEach((shape) => (shape.canMove = true));
-          }, 100);
+  let delay = 0;
+  for (let i = 0; i < count; i++) {
+    setTimeout(() => {
+      let shape;
+      let validPosition = false;
+
+      while (!validPosition) {
+        shape = new Shape(type);
+        validPosition = true;
+
+        for (const existingShape of shapesArray) {
+          if (existingShape.type === type) {
+            const dx = shape.x - existingShape.x;
+            const dy = shape.y - existingShape.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < 25) {
+              validPosition = false;
+              break;
+            }
+          }
         }
-      }, delay);
-      delay += 100;
-    }
+      }
+
+      shapesArray.push(shape);
+      if (i === count - 1) {
+        setTimeout(() => {
+          shapesArray.forEach((shape) => (shape.canMove = true));
+        }, 100);
+      }
+    }, delay);
+    delay += 100;
   }
+}
 
 const rockCount = 1 + Math.floor(Math.random() * 16);
 const paperCount = 1 + Math.floor(Math.random() * 16);
@@ -159,10 +179,30 @@ createShapes("paper", paperCount);
 createShapes("scissors", scissorsCount);
 
 function resetGame() {
-  shapesArray.length = 0
+  shapesArray.length = 0;
   createShapes("rock", rockCount);
   createShapes("paper", paperCount);
   createShapes("scissors", scissorsCount);
+  winnerCheckInterval = setInterval(checkWinner, 1000);
+  const winnerMessageElement = document.getElementById("winner-message");
+  winnerMessageElement.innerHTML = "";
+  const winnerImageElement = document.getElementById("winner-image");
+  winnerImageElement.style.display = "none";
+}
+
+function checkWinner() {
+  const shapeTypes = new Set();
+  shapesArray.forEach((shape) => shapeTypes.add(shape.type));
+
+  if (shapeTypes.size === 1) {
+    clearInterval(winnerCheckInterval);
+    const winnerImageElement = document.getElementById("winner-image");
+    const winnerType = Array.from(shapeTypes)[0];
+    winnerImageElement.src = images[winnerType].src;
+    winnerImageElement.style.display = "block";
+    const winnerMessageElement = document.getElementById("winner-message");
+    winnerMessageElement.innerHTML = `Game over! The winner is:`;
+  }
 }
 
 function animate() {
